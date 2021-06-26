1ebe75fac5315e8cb241a85c3402b2f9059267ee
kunal21raut <72223057+kunal21raut@users.noreply.github.com>
2021-06-26 21:11:07 +0530

    html file & styling added

--- quizapp/models.py
+++ quizapp/models.py
@@ -10,6 +10,7 @@ class Test(models.Model):
     active = models.BooleanField(default=True)
     test_time = models.CharField(max_length=3, default=None)
     
+    
 
     def __str__(self):
         return self.category
