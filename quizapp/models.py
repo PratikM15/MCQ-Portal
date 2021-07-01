c0c1daa692050edda440e22a4a9c97a225f60e07
kunal21raut <72223057+kunal21raut@users.noreply.github.com>
2021-07-01 16:12:51 +0530

    commit

--- quizapp/models.py
+++ quizapp/models.py
@@ -36,7 +36,7 @@ class Question(models.Model):
     choice4 = models.CharField(max_length=500)
     answer = models.CharField(max_length=1)
     test = models.ForeignKey(Test, on_delete=models.DO_NOTHING)
-    
+ 
     def __str__(self):
         return self.question
 
